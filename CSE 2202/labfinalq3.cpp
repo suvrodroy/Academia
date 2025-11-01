60a56f1e08cd6777ea911436da50fcaf86666321
master <suvrodroy@gmail.com>
2025-11-01 15:32:57 +0600

    maybe last in this semester

--- CSE 2202/labfinalq3.cpp
+++ CSE 2202/labfinalq3.cpp
@@ -14,13 +14,45 @@ typedef unordered_map<int,int> umap;
 #define rep(i,j,k) for(int i=j;i<k;i++)
 #define all(a) a.begin(),a.end()
 #define endl '\n'
-#define INF 2147483647
+#define INF 1000000
 //#define INF 9223372036854775807
 #define null '\0'
-const int N = 1e5 + 10, Mod = 1e9 + 7;
+const int N = 1e4 + 10, Mod = 1e9 + 7;
 int n,m;
+int dp[N];
+int f(int wt, vint &a)
+{
+    if (wt == 0) return 0;
+    if (dp[wt] != -1) return dp[wt];
+    int ans = INF;
+    for (auto w : a)
+    {
+        if (w <= wt)
+            ans = min(ans, f(wt - w, a) + 1);
+    }
+    return dp[wt] = ans;
+}
 void solve(){
-
+    memset(dp,-1,sizeof(dp));
+    cin >> n >> m;
+    vint a(n);
+    rep(i,0,n) cin >> a[i];
+    f(m, a);
+    vint coins;
+    while (m > 0)
+    {
+        for (auto w : a)
+        {
+            if (dp[m - w] + 1 == dp[m])
+            {
+                coins.push_back(w);
+                m -= w;
+                break;
+            }
+        }
+    }
+    cout << coins.size() << endl;
+    for (auto i : coins) cout << i << " "; cout << endl;
 }
 signed main() {
     ios_base::sync_with_stdio(false);
